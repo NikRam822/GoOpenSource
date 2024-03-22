215c82ba7367bed6314a61396f98d842dc8404e5
Nikita Ramzin <72182571+NikRam822@users.noreply.github.com>
2024-03-22 23:05:07 +0300

    Added first view (#1)
    * Added first view
    
    * Added vuetify and added vue components
    
    * Deleted assets entries
    
    * Updated readme
    
    ---------
    
    Co-authored-by: slome <slomeec@gmail.com>
    Co-authored-by: Timmmich19 <t.akhmatovya@yandex.ru>

---
+++ GoOpenSourceFront/src/components/ReposCards.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <v-container>
+            <v-card v-for="rep in repositories[0]" variant="tonal" class = "my-5" hover :href = 'rep'>
+                <v-card-item> {{ rep }}</v-card-item>
+            </v-card>
+        </v-container>
+    </div>
+</template>
+<script>
+export default {
+    props: {
+        repositories: {
+            type: Array,
+            default: () => []
+        }
+    }
+}
+</script>
