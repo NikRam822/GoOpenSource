195793b1bbe4d8a2723d92ee2de26a04cda87d5d
Timmmich19 <t.akhmatovya@yandex.ru>
2024-03-23 23:09:54 +0300

    Added validation and refactored search page

--- GoOpenSourceFront/src/pages/SearchPage.vue
+++ GoOpenSourceFront/src/pages/SearchPage.vue
@@ -1,32 +1,32 @@
 <template>
-    <SearchRepos @getRepositories = "getRepositories"></SearchRepos>
-    <ReposCards :repositories="repositories"></ReposCards>
-  </template>
-  <script>
-  import SearchRepos from '@/components/SearchRepos.vue';
-  import ReposCards from '@/components/ReposCards.vue';
-  
-  export default {
-    components: {
-      SearchRepos,
-      ReposCards
-    },
-    data() {
-      return {
-        repositories: [],
-      };
-    },
-    methods: {
-      async getRepositories(queryForProject) {
-        try {
-          const response = await axios.post('http://127.0.0.1:8000/getRepositories', {
-            queryForProject: queryForProject
-          });
-          this.repositories = response.data.repositories;
-        } catch (error) {
-          console.error('Error fetching repositories:', error);
-        }
+  <SearchRepos @getRepositories="getRepositories"></SearchRepos>
+  <ReposCards :repositories="repositories"></ReposCards>
+</template>
+<script>
+import SearchRepos from '@/components/SearchRepos.vue';
+import ReposCards from '@/components/ReposCards.vue';
+
+export default {
+  components: {
+    SearchRepos,
+    ReposCards
+  },
+  data() {
+    return {
+      repositories: [],
+    };
+  },
+  methods: {
+    async getRepositories(queryForProject) {
+      try {
+        const response = await axios.post('http://127.0.0.1:8000/getRepositories', {
+          queryForProject: queryForProject
+        });
+        this.repositories = response.data.repositories;
+      } catch (error) {
+        console.error('Error fetching repositories:', error);
       }
     }
   }
-  </script>
+}
+</script>
