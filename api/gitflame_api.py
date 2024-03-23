74e1a862081a2e7c07d3d02176c641e4913075d9
Timur Yakhshigulov <tyakhshigulov@ozon.ru>
2024-03-23 19:30:55 +0300

    gitflame integration

---
+++ api/gitflame_api.py
@@ -0,0 +1,31 @@
+import requests
+from github import Github
+from dotenv import load_dotenv
+import os
+
+from api.general_api import GitAPI
+
+
+class GitFlameAPI(GitAPI):
+    load_dotenv()
+
+    def get_repositories(self, query_for_project):
+        try:
+            print(self.__url())
+            repositories = requests.get(
+                url=self.__url(),
+                params={
+                    "page": 1,
+                    "limit": GitAPI.get_number_of_repos(),
+                    "q": query_for_project,
+                    "is_private": False,
+                })
+            repo_links = [repo["html_url"] for repo in repositories.json()["repositories"][:GitAPI.get_number_of_repos()]]
+            return repo_links
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            return []
+
+    @staticmethod
+    def __url() -> str:
+        return os.getenv("GITFLAME_API_URL") + "/search"
