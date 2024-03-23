7908680fa7f7c457c2390c2abc963889150ae4bf
Timur Yakhshigulov <tyakhshigulov@ozon.ru>
2024-03-23 23:01:40 +0300

    fill more information for github and gitlab

--- api/github_api.py
+++ api/github_api.py
@@ -11,16 +11,18 @@ from models.repository import Model, Contributor
 class GitHubAPI(GitAPI):
     load_dotenv()
 
+    def __init__(self):
+        self.g = Github(os.getenv("GITHUB_ACCESS_TOKEN"))
+
     def get_repositories(self, query_for_project) -> typing.List[Model]:
         try:
-            g = Github(os.getenv("GITHUB_ACCESS_TOKEN"))
-            repositories = g.search_repositories(query=query_for_project)
+            repositories = self.g.search_repositories(query=query_for_project)
             repo_links = [
                 Model(
                     link=repo.html_url,
                     clone_link=repo.clone_url,
                     name=repo.name,
-                    readme="",
+                    readme=self.get_readme(repo.owner.login, repo.name),
                     description=repo.description,
                     stars=repo.watchers_count,
                     contributors=[],
@@ -32,3 +34,6 @@ class GitHubAPI(GitAPI):
         except Exception as e:
             print(f"An error occurred: {e}")
             return []
+
+    def get_readme(self, owner: str, repo_name: str, ) -> str:
+        return self.g.get_repo(full_name_or_id=f"{owner}/{repo_name}",lazy=True).get_readme().decoded_content.decode()
