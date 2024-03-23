8c14ba4dca102c347c4b7f9d633ff8fafdb90020
Timur Yakhshigulov <tyakhshigulov@ozon.ru>
2024-03-23 21:21:56 +0300

    models for api

--- api/gitflame_api.py
+++ api/gitflame_api.py
@@ -1,15 +1,17 @@
 import os
+import typing
 
 import requests
 from dotenv import load_dotenv
 
 from api.general_api import GitAPI
+from models.repository import Model, Contributor
 
 
 class GitFlameAPI(GitAPI):
     load_dotenv()
 
-    def get_repositories(self, query_for_project):
+    def get_repositories(self, query_for_project) -> typing.List[Model]:
         try:
             repositories = requests.get(
                 url=self.search_url(),
@@ -19,8 +21,23 @@ class GitFlameAPI(GitAPI):
                     "q": query_for_project,
                     "is_private": False,
                 })
-            repo_links = [repo["html_url"] for repo in repositories.json()["repositories"][:GitAPI.get_number_of_repos()]]
-            return repo_links
+            models = [
+                Model(
+                    link=repo["html_url"],
+                    clone_link=repo["clone_url"],
+                    name=repo["name"],
+                    readme=self.__get_readme(repo["owner"]["username"], repo["name"]),
+                    description=repo["description"],
+                    stars=repo["stars_count"],
+                    contributors=[],
+                    owner=Contributor(
+                        username=repo["owner"]["username"],
+                    ),
+                )
+                for repo in
+                repositories.json()["repositories"][:GitAPI.get_number_of_repos()]]
+            return models
+
         except Exception as e:
             print(f"An error occurred: {e}")
             return []
@@ -28,3 +45,27 @@ class GitFlameAPI(GitAPI):
     @staticmethod
     def search_url() -> str:
         return os.getenv("GITFLAME_API_URL") + "/search"
+
+    @staticmethod
+    def readme_urls(owner: str, repo_name: str, ) -> typing.List[str]:
+        prefix = os.getenv("GITFLAME_API_URL") + f"/repos/{owner}/{repo_name}/raw//"
+        return [
+            prefix + "readme.md",
+            prefix + "README.md",
+            prefix + "ReadMe.md",
+        ]
+
+    def __get_readme(self, owner: str, repo_name: str, ) -> str:
+        try:
+            for link in self.readme_urls(owner, repo_name):
+                readme = requests.get(
+                    url=link)
+
+                if readme.status_code == 200:
+                    return readme.content.decode()
+
+            print(f"Can not find readme.md for gitflame {owner}/{repo_name}")
+            return ""
+        except Exception as e:
+            print(f"An error occurred during getting readme: {e}")
+            return ""
--- api/github_api.py
+++ api/github_api.py
@@ -1,21 +1,34 @@
+import typing
+
 from github import Github
 from dotenv import load_dotenv
 import os
 
 from api.general_api import GitAPI
+from models.repository import Model, Contributor
 
 
 class GitHubAPI(GitAPI):
-
     load_dotenv()
 
-    def get_repositories(self, query_for_project):
+    def get_repositories(self, query_for_project) -> typing.List[Model]:
         try:
             g = Github(os.getenv("GITHUB_ACCESS_TOKEN"))
             repositories = g.search_repositories(query=query_for_project)
-            repo_links = [repo.html_url for repo in repositories[:GitAPI.get_number_of_repos()]]
+            repo_links = [
+                Model(
+                    link=repo.html_url,
+                    clone_link="",
+                    name="",
+                    readme="",
+                    description="",
+                    stars=0,
+                    contributors=[],
+                    owner=Contributor(
+                        username=""
+                    ),
+                ) for repo in repositories[:GitAPI.get_number_of_repos()]]
             return repo_links
         except Exception as e:
             print(f"An error occurred: {e}")
             return []
-
