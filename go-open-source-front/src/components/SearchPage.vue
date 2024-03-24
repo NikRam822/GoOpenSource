9876a1e009b356443c0a8aabd6676eefcfab50ca
slome <slomeec@gmail.com>
2024-03-24 15:26:29 +0500

    added loader and text-area

--- go-open-source-front/src/components/SearchPage.vue
+++ go-open-source-front/src/components/SearchPage.vue
@@ -4,6 +4,9 @@
       <v-col>
         <v-container>
           <SearchRepos @getRepositories="getRepositories"></SearchRepos>
+          <v-col align="center">
+            <v-progress-circular v-if="switch" indeterminate size="50" width="10" color="grey-darken-3"></v-progress-circular>
+          </v-col>
           <ReposCards :repositories="repositories"></ReposCards>
         </v-container>
       </v-col>
@@ -18,17 +21,20 @@ import axios from 'axios'
 export default {
   data() {
     return {
+      switch: false,
       repositories: [],
     };
   },
   methods: {
     async getRepositories(queryForProject) {
+      this.switch = true
       try {
         const response = await axios.post('http://127.0.0.1:8000/getRepositories', {
           queryForProject: queryForProject
         }, {
         withCredentials: true,
       })
+        this.switch = false
         this.repositories = response.data.repositories;
       } catch (error) {
         console.error('Error fetching repositories:', error);
