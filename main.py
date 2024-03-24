d20a3c1df3099509db4b390359683647e2393b2e
Aslan <nurlybekov.aslan@yandex.kz>
2024-03-24 13:39:51 +0300

    origins2

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins2,
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allow_headers=["Accept",
