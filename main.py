3ef1003595a85738e9b44260aecd1719f4795ecc
Aslan <nurlybekov.aslan@yandex.kz>
2024-03-24 05:49:32 +0300

    cors orgins

--- main.py
+++ main.py
@@ -12,12 +12,19 @@ from api.moshub import MosHub
 
 app = FastAPI()
 
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:8000",
+    "http://localhost:5173",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["*"],  # Replace "*" with your allowed origins if needed
     allow_credentials=True,
-    allow_methods=["GET", "POST", "OPTIONS"],
-    allow_headers=["*"],
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],  # Adjust allowed methods as needed
+    allow_headers=["*"],  # Replace "*" with your allowed headers if needed
 )
 
 apis = {
