79f3b2ddccdc8c5ec8665796d3658611cfb68bc4
Nikita Ramzin <72182571+NikRam822@users.noreply.github.com>
2024-03-24 04:45:51 +0300

    Features/moshub model (#14)
    * Add links
    
    * Add links
    
    * Add model for mos hub

--- api/moshub.py
+++ api/moshub.py
@@ -16,21 +16,50 @@ class MosHub(GitAPI):
             url = "https://hub.mos.ru/explore/projects" + "?name=" + query_for_project + "&sort=latest_activity_desc"
             response = requests.get(url)
             soup = BeautifulSoup(response.content, 'html.parser')
-
             repo_links = [Model(
-                link="https://hub.mos.ru" + link['href'],
+                link="https://hub.mos.ru" + link.find_all('div', class_='project-cell gl-w-11')[0].find_all('a')[0][
+                    'href'],
                 clone_link="",
-                name="",
-                readme="",
+                name=link.find_all('div', class_='project-cell gl-w-11')[0].find_all('a')[0]['href'].split("/")[2],
+                readme=self.get_readme(
+                    link.find_all('div', class_='project-cell gl-w-11')[0].find_all('a')[0]['href'].split("/")[1],
+                    link.find_all('div', class_='project-cell gl-w-11')[0].find_all('a')[0]['href'].split("/")[2]),
                 description="",
-                stars=0,
+                stars=int(
+                    link.find_all('div', class_='controls gl-display-flex gl-align-items-center')[1].find_all('a')[
+                        0].text.replace("\n", "")),
                 contributors=[],
                 owner=Contributor(
-                    username=""
+                    username=link.find_all('div', class_='project-cell gl-w-11')[0].find_all('a')[0]['href'].split("/")[
+                        1]
                 ),
             )
-                for link in soup.find_all('a', class_='project')[:GitAPI.get_number_of_repos()]]
+                for link in soup.find_all('li', class_='project-row')[:GitAPI.get_number_of_repos()]]
             return repo_links
         except Exception as e:
             print(f"An error occurred: {e}")
             return []
+
+    def readme_urls(self, owner: str, repo_name: str, ) -> typing.List[str]:
+        prefix = f"https://hub.mos.ru/{owner}/{repo_name}/-/raw/"
+        return [
+            prefix + "main/README.md",
+            prefix + "main/readme.md",
+            prefix + "main/ReadMe.md",
+            prefix + "master/README.md",
+            prefix + "master/readme.md",
+            prefix + "master/ReadMe.md",
+        ]
+
+    def get_readme(self, owner: str, repo_name: str, ) -> str:
+        try:
+            for link in self.readme_urls(owner, repo_name):
+                readme = requests.get(
+                    url=link)
+                if readme.status_code == 200:
+                    return readme.content.decode()
+            print(f"Can not find readme.md for gitverse {owner}/{repo_name}")
+            return ""
+        except Exception as e:
+            print(f"An error occurred during getting readme: {e}")
+            return ""
