31c405843090b277678d62ae5321a730746ed9de
Nikita Ramzin <rivorn73@gmail.com>
2024-03-24 10:10:43 +0300

    Add keywords

--- ai/repo_keywords.py
+++ ai/repo_keywords.py
@@ -5,6 +5,6 @@ def extract_keywords(input: str) -> typing.List[str]:
     different_keywords = input.split(";")
 
     return [
-        # *different_keywords,  # отдельные кейворды
+        *different_keywords,  # отдельные кейворды
         " ".join(different_keywords),  # общий запрос, вдруг что-нибудь найдём
     ]
